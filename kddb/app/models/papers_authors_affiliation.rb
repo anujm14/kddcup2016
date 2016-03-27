24651a49b14d70d3a3d4412e05a58ee30d5dfbfe
Mike Ghen <mike.ghen@gmail.com>
2016-03-26 20:24:30 -0400

    Score functions and better view of the data in welcome#index

--- kddb/app/models/papers_authors_affiliation.rb
+++ kddb/app/models/papers_authors_affiliation.rb
@@ -2,4 +2,17 @@ class PapersAuthorsAffiliation < ActiveRecord::Base
   belongs_to :paper, primary_key: "paper_id"
   belongs_to :author, primary_key: "author_id"
   belongs_to :affiliation, primary_key: "affiliation_id"
+
+  PAPER_POINT = 1.0
+
+  def author_score
+    PAPER_POINT / PapersAuthorsAffiliation.where(paper_id: paper_id,
+                                                 author_id: author_id).count
+  end
+  
+  def affiliation_score
+    author_score / PapersAuthorsAffiliation.where(paper_id: paper_id,
+                                                  author_id: author_id,
+                                                  affiliation_id: affiliation_id).count
+  end
 end
